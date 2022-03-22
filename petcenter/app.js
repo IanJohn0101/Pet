bc1f7e8f4066a4618fe55ba6256d20e4cb6d25a0
JudahMae <judahmae02@gmail.com>
2022-03-22 11:41:07 +0800

    as

--- petcenter/app.js
+++ petcenter/app.js
@@ -1,8 +1,16 @@
 function initMap(){
+    //map otion
     var options = {
         center: {lat:10.3157, lng:-123.8854},
-        zoom: 8,    
+        zoom: 9,    
     }
 
+    //new map
     map = new google.maps.Map(document.getElementById('map'), options);
+
+    //marker
+    const marker = new google.maps.Marker({
+        position: {lat: 10.305473, lng: -124.011226},
+        map: map
+    });
 }
